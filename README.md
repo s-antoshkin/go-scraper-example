0e38013b7f27c4266c5fdb8d87c948342da476d7
Semyon Antoshkin <s.s.antoshkin@gmail.com>
2023-01-25 23:12:06 +0500

    Update README.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# Golang scraper example
+Небольшой скрипт для скрапинга сайта [books.toscrape.com](https://books.toscrape.com/) (пакет `goquery`)
+- сбор информации о книгах: наименование, ссылка на страницу книги, цена, наличие;
+- запись полученной информации в `csv-файл`.
+
+## Requirements
+---
+Go 1.19 or above.
+
+## Installation
+---
+```
+git clone https://github.com/s-antoshkin/go-scraper-example
+cd go-scraper-example
+go get -u
+```
+
+## Start the application
+---
+```
+go run .
+```
+
+## Packages used
+---
+```
+"encoding/csv"
+"fmt"
+"net/http"
+"os"
+"regexp"
+"strings"
+"time"
+
+"github.com/PuerkitoBio/goquery"
+```
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const BASE_URL = "http://books.toscrape.com/"
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	pages := make(chan int, 50)
+	results := make(chan []string)
+	file, err := os.Create("books.csv")
+	check(err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	writer.Comma = ';'
+	defer writer.Flush()
+
+	start := time.Now()
+
+	for i := 1; i <= cap(pages); i++ {
+		go scraping(pages, results)
+	}
+
+	go func() {
+		for i := 1; i <= cap(pages); i++ {
+			pages <- i
+		}
+	}()
+
+	for i := 1; i <= 1000; i++ {
+		result := <-results
+
+		if err := writer.Write(result); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	close(pages)
+	close(results)
+
+	fmt.Printf("End: %s", time.Since(start))
+
+}
+
+func scraping(pages chan int, result chan []string) {
+	for i := range pages {
+		res, err := http.Get(BASE_URL + fmt.Sprintf("catalogue/page-%d.html", i))
+		check(err)
+
+		if res.StatusCode > 400 {
+			fmt.Println("Status code: ", res.StatusCode)
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		check(err)
+
+		s := "article.product_pod"
+		doc.Find(s).Each(func(i int, s *goquery.Selection) {
+			h3A := s.Find("h3 a")
+			url, ok := h3A.Attr("href")
+			if !ok {
+				return
+			}
+			fullURL := BASE_URL + url
+
+			title, ok := h3A.Attr("title")
+			if !ok {
+				return
+			}
+			stringPrice := strings.TrimSpace(s.Find("div.product_price p.price_color").Text())
+			re := regexp.MustCompile(`\d+.\d+`)
+			price := re.Find([]byte(stringPrice))
+
+			available := strings.TrimSpace(s.Find("div.product_price p.instock").Text())
+
+			book := []string{title, fullURL, string(price), available}
+			result <- book
+
+		})
+
+		res.Body.Close()
+	}
+
+}
